77589e1cda2e7edcf657c930aaac0da160edf779
华霆 <584425439@qq.com>
2019-11-25 17:27:51 +0800

    更新模型基类

--- chatApi/app/Models/Base.py
+++ chatApi/app/Models/Base.py
@@ -3,7 +3,7 @@
 @Date: 2019-06-17 14:14:28
 @description: 基础模型，封装一些基础方法 
 @LastEditors: hua
-@LastEditTime: 2019-10-30 09:07:59
+@LastEditTime: 2019-11-25 17:25:36
 '''
 import logging, math
 from app.Vendor.Code import Code
@@ -12,18 +12,17 @@ from app import dBSession
 
 class Base():
 
-    """ 
+    def getList(self, cls_:object, filters:set, order:str="id desc", field:tuple=(), offset:int = 0, limit:int = 15)->dict:
+        """ 
         列表
         @param object cls_ 数据库模型实体类
         @param set filters 查询条件
-        @param obj order 排序
+        @param str order 排序
         @param tuple field 字段
         @param int offset 偏移量
         @param int limit 取多少条
         @return dict
-    """
-    
-    def getList(self, cls_, filters, order, field=(), offset = 0, limit = 15):
+        """
         res = {}
         res['page'] ={}
         res['page']['count'] = dBSession.query(cls_).filter(*filters).count()
@@ -35,112 +34,119 @@ class Base():
 
         if res['page']['count'] > 0:
             res['list'] = dBSession.query(cls_).filter(*filters)
-            res['list'] = res['list'].order_by(order).offset(offset).limit(limit).all()
+            orderArr = order.split(' ')
+            if orderArr[1] == 'desc':
+                res['list'] = res['list'].order_by(desc(orderArr[0])).offset(offset).limit(limit).all()
+            else:
+                res['list'] = res['list'].order_by(asc(orderArr[0])).offset(offset).limit(limit).all()
         if not field:
             res['list'] = [c.to_dict() for c in res['list']]
         else:
             res['list'] = [c.to_dict(only=field) for c in res['list']]
         return res
 
-    """
+    def getAll(self, cls_:object, filters:set, order:str = 'id desc', field:tuple = (), limit:int = 0)->list:
+        """
         查询全部
         @param object cls_ 数据库模型实体类
         @param set filters 查询条件
-        @param obj order 排序
+        @param str order 排序
         @param tuple field 字段
         @param int $limit 取多少条
         @return dict
-    """
-    def getAll(self, cls_, filters, order = 'id desc', field = (), limit = 0):
+        """
         if not filters:
             res = dBSession.query(cls_)
         else:   
             res = dBSession.query(cls_).filter(*filters)
         if limit != 0:
             res = res.limit(limit)
-        order = order.split(' ')
-        if order[1] == 'desc':
-            res = res.order_by(desc(order[0])).all()
+        orderArr = order.split(' ')
+        if orderArr[1] == 'desc':
+            res = res.order_by(desc(orderArr[0])).all()
         else:
-            res = res.order_by(asc(order[0])).all()
+            res = res.order_by(asc(orderArr[0])).all()
         if not field:
             res = [c.to_dict() for c in res]
         else:
             res = [c.to_dict(only=field) for c in res]
         return res
 
-    """
+    def getOne(self, cls_:object, filters:set, order:str = 'id desc', field :tuple= ()):
+        """
         获取一条
         @param object cls_ 数据库模型实体类
         @param set filters 查询条件
-        @param obj order 排序
+        @param str order 排序 
         @param tuple field 字段
         @return dict
-    """
-    def getOne(self, cls_, filters, order = 'id desc', field = ()):
+        """
         res = dBSession.query(cls_).filter(*filters)
-        order = order.split(' ')
-        if order[1] == 'desc':
-            res = res.order_by(desc(order[0])).first()
+        orderArr = order.split(' ')
+        if orderArr[1] == 'desc':
+            res = res.order_by(desc(orderArr[0])).first()
         else:
-            res = res.order_by(asc(order[0])).first()
+            res = res.order_by(asc(orderArr[0])).first()
         if res == None:
             return None
         if not field:
-            res = res.to_dict()
+            res = res.to_dict() 
         else:
            res = res.to_dict(only=field) 
         return res
   
-    """
+    def add(self, cls_, data:dict)->int:
+        """
         添加
         @param object cls_ 数据库模型实体类
-        @param obj data 数据
+        @param dict data 数据
         @return bool
-    """
-    def add(self, cls_, data):
+        """
         users = cls_(**data)
         dBSession.add(users)
         dBSession.flush()
         return users.id
 
-    """
+    def edit(self, cls_:object, data:dict, filters:set)->bool:
+        """
         修改
         @param object cls_ 数据库模型实体类
         @param dict data 数据
         @param set filters 条件
         @return bool
-    """
-    def edit(self, cls_, data, filters):
-        dBSession.query(cls_).filter(*filters).update(data, synchronize_session=False)
-        return True
+        """
+        return dBSession.query(cls_).filter(*filters).update(data, synchronize_session=False)
     
-    """
+    def delete(self, cls_:object, filters:set)->int:
+        """
         删除
         @param object cls_ 数据库模型实体类
-        @paramset filters 条件
-        @return bool
-    """
-    def delete(self, cls_, filters):
-        dBSession.query(cls_).filter(*filters).delete(synchronize_session=False)
-        return True
+        @param set filters 条件
+        @return int
+        """
+        return dBSession.query(cls_).filter(*filters).delete(synchronize_session=False)
     
-    """
+    def getCount(self, cls_:object, filters:set, field = None)->int:
+        """
         统计数量
         @param object cls_ 数据库模型实体类
         @param set filters 条件
         @param obj field 字段
         @return int
-    """  
-    def getCount(self, cls_, filters, field = None):
+        """  
         if field == None:
             return dBSession.query(cls_).filter(*filters).count()
         else:
             return dBSession.query(cls_).filter(*filters).count(field)
         
     @staticmethod
-    def get_page_number(count, page_size):
-        count = float(count)
+    def get_page_number(count:int, page_size:int)->int:
+        """ 
+        * 获取总页数
+        * @param int count 
+        * @param int page_size
+        * @return int 
+        """
         page_size = abs(page_size)
         if page_size != 0:
             total_page = math.ceil(count / page_size)
@@ -148,15 +154,15 @@ class Base():
             total_page = math.ceil(count / 5)
         return total_page
     
-    """ 
-    * 格式化分页
-    * @param int page
-    * @param int size
-    * @param int total
-    * @return dict 
-    """
     @staticmethod
-    def formatPaged(page, size, total):
+    def formatPaged(page:int, size:int, total:int)->dict:
+        """ 
+        * 格式化分页
+        * @param int page
+        * @param int size
+        * @param int total
+        * @return dict 
+        """
         if int(total) > int(page) * int(size):
             more = 1
         else:
@@ -168,13 +174,15 @@ class Base():
             'more': more
         }
 
-    """ 
-    * 格式化返回体
-    * @param dict data
-    * @return dict
-    """
     @staticmethod
-    def formatBody(data={}, msg='', show=True):
+    def formatBody(data:dict={}, msg:str='', show:bool=True)->dict:
+        """ 
+        * 格式化返回体
+        * @param dict data
+        * @param str  msg
+        * @param bool show
+        * @return dict
+        """
         dataformat = {}
         dataformat['error_code'] = Code.SUCCESS
         dataformat['data'] = data
@@ -182,14 +190,15 @@ class Base():
         dataformat['show'] = show
         return dataformat
 
-    """ 
-    * 格式化错误返回体
-    * @param int code
-    * @param string message
-    * @return dict
-    """
     @staticmethod
-    def formatError(code, message='', show=True):
+    def formatError(code:int, message:str='', show:bool=True)->dict:
+        """ 
+        * 格式化错误返回体
+        * @param int code
+        * @param str message
+        * @param bool show
+        * @return dict
+        """
         if code == Code.BAD_REQUEST:
             message = 'Bad request.'
         elif code == Code.NOT_FOUND:
