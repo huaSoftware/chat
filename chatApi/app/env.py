d96ac5f6fa9964f001e071b2766d8cbc3abafdd8
华霆 <584425439@qq.com>
2020-01-21 11:21:47 +0800

    修改pymysql驱动，使用mysqldb

--- chatApi/app/env.py
+++ chatApi/app/env.py
@@ -2,10 +2,10 @@
 @Author: hua
 @Date: 2019-02-10 09:55:10
 @LastEditors  : hua
-@LastEditTime : 2020-01-07 19:53:37
+@LastEditTime : 2020-01-21 11:13:21
 '''
 #mysql
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:mysql123@212.64.83.121:8066/chat?charset=utf8'
+SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root:mysql123@212.64.83.121:8066/chat?charset=utf8'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 #debug
 DEBUG_LOG = True
