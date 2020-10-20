bdc4fd0e6cba30a6d1b8544d56c7dd75ba0620d9
华霆 <584425439@qq.com>
2020-10-20 19:37:00 +0800

    添加缺少文件

---
+++ chatAdmin/public/static/js/star.js
@@ -0,0 +1,38 @@
+/*
+ * @Author: hua
+ * @Date: 2019-10-06 19:47:06
+ * @description:
+ * @LastEditors: hua
+ * @LastEditTime: 2020-05-04 11:46:18
+ */
+console.log("工具鸡 chat  https://gitee.com/huashiyuting/Tool-Chicken-Chat");
+console.log("工具鸡 flask   https://gitee.com/huashiyuting/flask");
+console.log("工具鸡 plainCms  https://gitee.com/huashiyuting/plainCms");
+console.log("联系作者  584425439@qq.com");
+console.log("请不要吝啬您的 star，谢谢 ~");
+
+console.log(
+  [
+    "%c                                                                            ",
+    "                                                                            ",
+    "                                                                            ",
+    "                               %c REC WARNING %c                                ",
+    "                                                                            ",
+    "                                                                            ",
+    "%c        请不要试图阅读本demo的源码，不然你会去想这么丑的代码是谁写的        ",
+    "              本文件包括附属的js文件的代码是经过长时间积累出来的            ",
+    "              代码虽然已经分层/分开写了，也许者作者也已经不认识了           ",
+    "                                                                            ",
+    "              如果想要快速入门，请阅读github项目内首页README文档            ",
+    "                     参考文档内的快速使用部分，简单快捷高效                 ",
+    "                                                                            ",
+    "                请不要试图阅读本demo的源码，正常情况下意义不大              ",
+    "                                                                            ",
+    "                                                                            ",
+    "",
+  ].join("\n"),
+  "background: #000; font-size: 18px; font-family: monospace",
+  "background: #f33; font-size: 18px; font-family: monospace; color: #eee; text-shadow:0 0 1px #fff",
+  "background: #000; font-size: 18px; font-family: monospace",
+  "background: #000; font-size: 18px; font-family: monospace; color: #ddd; text-shadow:0 0 2px #fff"
+);
